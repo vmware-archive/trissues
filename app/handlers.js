a3eaf2f94d0e1e2fbf13aeab9aa6bc2b135f859e
Jeff Turley <jturley@pivotallabs.com>
2015-02-26 10:23:06 -0700

    Refactor to use xml library

--- app/handlers.js
+++ app/handlers.js
@@ -1,62 +1,59 @@
 var restify = require("restify"),
-    config = require("environmental").config(),
-    exports = {
-      githubissues: function (req, res, next) {
-        var client = restify.createJsonClient({
-          url: "https://api.github.com/",
-          headers: {
-            Authorization: "token " + config.auth.github
-          }
-        }),
-            filteredLabels = (function () {
-              if (config.filteredlabels) {
-                return config.filteredlabels.split(/, */);
-              } else {
-                return [];
-              }
-            }());
-        client.get("/repos/" + config.github.repo + "/issues", function (err, githubReq, githubRes, issues) {
-          var xmlResponse = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><external_stories type=\"array\">";
-          issues.forEach(function (issue) {
-            if (issue.labels.some(
-              function (label) {
-                return filteredLabels.indexOf(label.name) !== -1;
-              })) {
-              return;
-            }
+config = require("environmental").config(),
+xml = require("xml");
 
-            xmlResponse += "<external_story>"
-              + "<external_id>" + issue.number + "</external_id>"
-              + "<story_type>feature</story_type>"
-              + "<name>" + issue.title + "</name>"
-              + "<requested_by>" + issue.user.login + "</requested_by>"
-              + "<created_at type=\"datetime\">" + issue.created_at + "</created_at>";
-
-            if ((issue.body || "") !== "") {
-              xmlResponse += "<description>" + issue.body + "</description>";
-            }
-
-            if (issue.assignee) {
-              xmlResponse += "<owned_by>" + issue.assignee.login + "</owned_by>";
-            }
-
-            xmlResponse += "</external_story>";
-          });
-
-          xmlResponse += "</external_stories>";
-
-          res.contentType = "application/xml";
-          res.send(200, xmlResponse);
-          return next();
-        });
-      },
-
-
-      fromtracker: function (req, res, next) {
-//console.log(req.body);
-        res.send(200);
-        return next();
+module.exports = {
+  githubissues: function (req, res, next) {
+    var client = restify.createJsonClient({
+      url: "https://api.github.com/",
+      headers: {
+        Authorization: "token " + config.auth.github
       }
-    };
+    }),
+    filteredLabels = (function () {
+      if (config.filteredlabels) {
+        return config.filteredlabels.split(/, */);
+      } else {
+        return [];
+      }
+    }());
+    client.get("/repos/" + config.github.repo + "/issues", function (err, githubReq, githubRes, issues) {
+      var responseObj = {
+        external_stories: [{ _attr: { type: "array" } }]
+      };
 
-module.exports = exports;
+      issues.forEach(function (issue) {
+        if (!issue.labels.some(
+          function (label) {
+            return filteredLabels.indexOf(label.name) !== -1;
+          })) {
+          var externalStory = [
+            { external_id: issue.number },
+            { story_type: "feature" },
+            { name: issue.title },
+            { requested_by: issue.user.login },
+            { created_at: [{ _attr:{ type: "datetime" } }, issue.created_at] }
+          ];
+
+          if ((issue.body || "") !== "") {
+            externalStory.push({ description: issue.body });
+          }
+
+          if (issue.assignee) {
+            externalStory.push({ owned_by: issue.assignee.login });
+          }
+          responseObj.external_stories.push({ external_story: externalStory });
+        }
+      });
+
+      res.contentType = "application/xml";
+      res.send(200, xml(responseObj, { declaration: true }));
+      return next();
+    });
+  },
+  fromtracker: function (req, res, next) {
+//console.log(req.body);
+    res.send(200);
+    return next();
+  }
+};
