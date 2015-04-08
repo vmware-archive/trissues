e7a1335e32fb9f06c1b075c27f59cc9b1cde1e7f
Jeff Turley <jturley@pivotal.io>
2015-04-07 20:45:09 -0600

    fix indentation

--- app/helpers.js
+++ app/helpers.js
@@ -2,9 +2,9 @@
 var helpers;
 
 helpers = {
-    log: function (message) {
-      console.log(message);
-    }
+  log: function (message) {
+    console.log(message);
+  }
 };
 
 module.exports = helpers;
